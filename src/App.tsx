a76d720e7a64dfba619f356d606ff08aa1508ec7
John Clampet <jsclampet@gmail.com>
2024-01-16 02:33:25 -0600

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,8 @@
-import { useEffect, useState } from "react";
-import { JSON } from "./data";
-import { CartesianGrid, XAxis, YAxis, Tooltip, BarChart, Bar } from "recharts";
-import All_Chart from "./components/All_Chart/All_Chart";
 import TSR_CSR_weekly from "./components/TSR_CSR_weekly/TSR_CSR_weekly";
 
-interface TicketDataType {
-  name: string;
-  count: number;
-}
-
 const App = () => {
-  const [ticketDataArray, setTicketDataArray] = useState<TicketDataType[]>([]);
-  useEffect(() => {
-    const ticketInfoObject = () => {
-      let openCount: number = 0;
-      let closedCount: number = 0;
-      let pendingExternalCount: number = 0;
-      let pendingInternalCount: number = 0;
-      JSON.forEach((ticket) => {
-        switch (ticket.status) {
-          case "OPEN":
-            openCount++;
-            break;
-          case "CLOSED":
-            closedCount++;
-            break;
-          case "PENDING_EXTERNAL":
-            pendingExternalCount++;
-            break;
-          case "PENDING_INTERNAL":
-            pendingInternalCount++;
-            break;
-          default:
-            break;
-        }
-      });
-      setTicketDataArray([
-        { name: "Closed", count: closedCount },
-        { name: "Open", count: openCount },
-        {
-          name: "Pending Ext",
-          count: pendingExternalCount,
-        },
-        {
-          name: "Pending Int",
-          count: pendingInternalCount,
-        },
-      ]);
-    };
-    ticketInfoObject();
-  }, []);
-
   return (
     <div>
-      <h2>Tickets by Status || any date range ???? </h2>
-      <BarChart width={500} height={300} data={ticketDataArray}>
-        <XAxis dataKey="name" stroke="#8884d8" />
-        <YAxis />
-        <Tooltip wrapperStyle={{ width: 100, backgroundColor: "#ccc" }} />
-        <CartesianGrid stroke="#ccc" strokeDasharray="8 5" />
-        <Bar dataKey="count" fill="#8884d8" barSize={30} />
-      </BarChart>
-      <br />
-      <hr />
-
-      <h2>Tickets by Department</h2>
-      <h3>JAN 1 - JAN 14</h3>
-      <br />
-      <All_Chart />
-      <br />
-      <hr />
-
       <h2>Tickets by Week | January</h2>
       <br />
       <TSR_CSR_weekly />
--- src/components/TSR_CSR_weekly/TSR_CSR_weekly.tsx
+++ src/components/TSR_CSR_weekly/TSR_CSR_weekly.tsx
@@ -1,34 +1,64 @@
-import { Bar, BarChart, CartesianGrid, Tooltip, XAxis, YAxis } from "recharts";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 import { data } from "./TSR_CSR_weekly.data";
 
 const TSR_CSR_weekly = () => {
-  let week1Count: number = 0;
-  let week2Count: number = 0;
-  const ticketCreatedDay = (day: string) => {
-    return parseInt(day.split("T")[0].split("-")[2]);
+  const week1 = {
+    week: "1/1/23 through 1/7/23",
+    csrTicketCount: 0,
+    tsrTicketCount: 0,
   };
-  data.forEach((ticket) => {
-    if (ticketCreatedDay(ticket.created_at) <= 7) {
-      week1Count++;
-    } else if (ticketCreatedDay(ticket.created_at) < 15) {
-      week2Count++;
+  const week2 = {
+    week: "1/8/23 through 1/14/23",
+    csrTicketCount: 0,
+    tsrTicketCount: 0,
+  };
+  const dayOfMonth = (date: string) => {
+    return parseInt(date.split("T")[0].split("-")[2]);
+  };
+  const csrTickets = data.filter(
+    (ticket) =>
+      ticket.ticket_group_id === "19" || ticket.ticket_group_id === "18"
+  );
+  const tsrTickets = data.filter(
+    (ticket) =>
+      ticket.ticket_group_id === "17" || ticket.ticket_group_id === "3"
+  );
+  csrTickets.forEach((ticket) => {
+    if (dayOfMonth(ticket.created_at) <= 7) {
+      week1.csrTicketCount++;
+    } else if (dayOfMonth(ticket.created_at) < 14) {
+      week2.csrTicketCount++;
+    }
+  });
+  tsrTickets.forEach((ticket) => {
+    if (dayOfMonth(ticket.created_at) <= 7) {
+      week1.tsrTicketCount++;
+    } else if (dayOfMonth(ticket.created_at) < 14) {
+      week2.tsrTicketCount++;
     }
   });
 
-  const allTicketData = [
-    { week: "1/1/23 - 1/7/23", count: week1Count },
-    { week: "1/8/23 - 1/14/23", count: week2Count },
-  ];
+  const allTicketData = [week1, week2];
 
   return (
     <div>
       <div>
-        <BarChart width={900} height={300} data={allTicketData}>
-          <XAxis dataKey="week" stroke="#8884d8" />
+        <BarChart width={600} height={400} data={allTicketData}>
+          <XAxis dataKey="week" stroke="#ccc" />
           <YAxis />
           <Tooltip />
           <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-          <Bar dataKey="count" fill="#8884d8" barSize={30} />
+          <Legend />
+          <Bar dataKey="csrTicketCount" fill="#8884d8" barSize={30} />
+          <Bar dataKey="tsrTicketCount" fill="#82ca9d" barSize={30} />
         </BarChart>
       </div>
     </div>
